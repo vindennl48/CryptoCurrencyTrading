edceebad543e58c9cc63550d50ef2f6d2a852ac9
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2018-07-22 14:50:11 +0000

    Added email to users, Started authorization
    trying to prevent someone from seeing other peoples API'S

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,17 +3,26 @@ require 'binance'
 
 class DashboardController < ApplicationController
   before_action :auth_me
+  before_action :has_api
 
   def index
     @symbols = Api.get_symbols
     @assets = Api.get_assets(@symbols)
     @investment = Transaction.sum(:amount)
+
+    @debug = User.find(current_user.id).inspect
   end
 
   private
     def auth_me
       if not current_user
         redirect_to user_google_oauth2_omniauth_authorize_path
+      end
+    end
+
+    def has_api
+      if not Api.all
+        redirect_to apis_path
       end
     end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
       user.token = auth.credentials.token
       user.expires = auth.credentials.expires
       user.expires_at = auth.credentials.expires_at
